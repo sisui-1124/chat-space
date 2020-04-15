ce1fa1f53dc5bd0971c0cf5238eb4ef902dd1b8f
sisui-1124 <61412690+sisui-1124@users.noreply.github.com>
2020-04-15 17:25:08 +0900

    message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,13 +17,13 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+     @group = Group.find(params[:id])
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -35,6 +35,3 @@ class GroupsController < ApplicationController
   end
   
 end
-
-
-
